60529bf00e4e5fa57daaab357a4ff0cf350c6e27
Andres Leon Rangel <aleon1220@gmail.com>
2020-06-08 17:24:20 +1200

    updated description

--- docker-compose-actions.py
+++ docker-compose-actions.py
@@ -15,10 +15,6 @@ Describes the functions of the script
 
 """.format(script_name=os.path.basename(sys.argv[0])))
 
-
-################################################################################
-# Shared functionality
-
 def cmd_info():
     print("Showing MySQL ports, Deployed environment and Container name...")
     print("\n| MySQL Port |      Container     |     Environment Name   ")
@@ -28,15 +24,16 @@ def cmd_info():
     db_container_inspect = ranqxlib.subp_capture("docker inspect " + " ".join(db_containers), splitlines=False)
     for db_container_json in json.loads(db_container_inspect):
         print("| {:10} |  {:16.16}  | {}".format(
-            db_container_json['Config']['Labels']['com.ranqx.loan.port'],
+            db_container_json['Config']['Labels']['com.example.port'],
             db_container_json['Name'][1:],
-            db_container_json['Config']['Labels']['com.ranqx.loan.envname'],
+            db_container_json['Config']['Labels']['com.example.envname'],
             ))
+ 
     # Example of format with 2 parameters    
     print("Branch {} File ID located at {} ".format(branch.name, str(branch.id_file)))
             
 ################################################################################
-# Main Execution
+# Execution
 
 try:
     aws_account = ranqxlib.get_aws_account()
@@ -59,4 +56,4 @@ try:
 except Exception as e:
     print("***Error:")
     traceback.print_exc()
-    sys.exit(1) 
+    sys.exit(1)
